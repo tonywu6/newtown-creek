a08f8bbc534e31612581cb8ab3e0c3dbd5caad12
Tony Wu <tony.wu@nyu.edu>
2020-03-05 10:56:28 -0500

    WIP zooming

---
+++ web/script/post-theory-making-2.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+function zoom(element) {
+    var actor = $(element).siblings('figure').children('.actor-container')
+    if (actor.length < 1) return
+
+    actor = actor[0]
+    $(actor).toggleClass('zoomed-in')
+    $(actor).toggleClass('zoomed-out')
+    actor.offsetWidth;
+
+    if ($(actor).hasClass('zoomed-in')) {
+        $(element).children('a').text('Zoom out')
+        $(element).addClass('color-red-bg')
+        $(element).removeClass('color-green-bg')
+
+        actor.offsetWidth
+    } else {
+        $(element).children('a').text('Zoom in')
+        $(element).removeClass('color-red-bg')
+        $(element).addClass('color-green-bg')
+
+        actor.offsetWidth
+    }
+}
