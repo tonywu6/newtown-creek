dfd7458b3fd0f24656104fbf76d851c29d10447f
monotony113 <tonywuyt126@gmail.com>
2020-04-28 22:20:33 +0800

    thumbnails

--- dmtp/__main__.py
+++ dmtp/__main__.py
@@ -14,6 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
 import click
 
 from . import assemble
@@ -40,6 +45,17 @@ def publish():
     assemble.build()
     assemble.publish()
 
+
+@cli.command()
+def make_thumbnails():
+    shutil.rmtree('static/thumbnails', ignore_errors=True)
+    for dirname, _, files in os.walk('static/img'):
+        for f in files:
+            p = Path(f'{dirname}/{f}')
+            rp = p.relative_to('static/img')
+            thp = Path('static/thumbnails', rp)
+            os.makedirs(thp.parent, exist_ok=True)
+            subprocess.run(['ffmpeg', '-i', str(p), '-pix_fmt', 'yuvj444p', '-vf', 'crop=in_h*0.8:in_h*0.8:(in_w-in_h*0.8)/2:in_h*0.1, scale=-1:768', str(thp)])
 
 if __name__ == '__main__':
     cli()
